c06aa79b68b8015aa31bcee9aa4f3b5f414b263d
Abhaya Kirtivasan <abhaya@Abhayas-MacBook-Pro.local>
2022-12-04 14:16:29 -0500

    added item table in profile component

---
+++ DONS365-Dons365_v113/client/src/components/Profile.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn, MDBTypography, MDBIcon } from 'mdb-react-ui-kit';
+
+export default function ProfileStatistics() {
+  return (
+    <div className="vh-100" style={{ backgroundColor: '#eee' }}>
+      <MDBContainer className="container py-5 h-100">
+        <MDBRow className="justify-content-center align-items-center h-100">
+          <MDBCol md="12" xl="4">
+            <MDBCard style={{ borderRadius: '15px' }}>
+              <MDBCardBody className="text-center">
+                <div className="mt-3 mb-4">
+                  <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp"
+                    className="rounded-circle" fluid style={{ width: '100px' }} />
+                </div>
+                <MDBTypography tag="h4">Name</MDBTypography>
+                <MDBCardText className="text-muted mb-4">
+                  @Student at PFW<span className="mx-2"></span> <a href="#!"></a>
+          
+                </MDBCardText>
+              </MDBCardBody>
+            </MDBCard>
+            <br></br>
+            <MDBCol lg="15">
+            <MDBCard className="mb-4">
+              <MDBCardBody>
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Items</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">Item name</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Price</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">Price of item</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
+                    <MDBCardText>Actions</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">action to be taken</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+              </MDBCardBody>
+            </MDBCard>
+            </MDBCol>
+          </MDBCol>
+        </MDBRow>
+        
+      </MDBContainer>
+      
+    </div>
+  );
+}
